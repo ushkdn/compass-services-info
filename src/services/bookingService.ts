55d62690f74987bb2d5a15e8a25b80c6cd602045
ushkdn <ushkandn@gmail.com>
2023-08-29 17:14:26 +0300

    bookingUpdate fix

--- src/services/bookingService.ts
+++ src/services/bookingService.ts
@@ -34,19 +34,32 @@ class bookingService {
     }
     return service.appointment.booking
   }
-  async update(serviceId: string, bookingId: string, updatedBooking: updateBookingDto) {
-    const service = await serviceModel.findById(serviceId)
-    if (!service) {
+  async update(serviceId: string, bookingId: string, updatedBookingFields: updateBookingDto) {
+    const updatedBookingQuery = {
+      $set: {
+        'appointment.booking.$[booking].dateStart': updatedBookingFields.dateStart,
+        'appointment.booking.$[booking].dateEnd': updatedBookingFields.dateEnd,
+      },
+    }
+
+    const options = {
+      arrayFilters: [{ 'booking._id': { $eq: bookingId } }],
+    }
+
+    const updatedService = await serviceModel.findByIdAndUpdate(serviceId, updatedBookingQuery, {
+      new: true,
+      arrayFilters: options.arrayFilters,
+    })
+    if (!updatedService) {
       throw new Error('Такая услуга не найдена')
     }
-    const bookingIndex = service.appointment.booking.findIndex((booking) => booking._id.toString() === bookingId)
-    if (!service.appointment.booking[bookingIndex]) {
+
+    const updatedBooking = updatedService.appointment.booking.find((booking) => booking._id.toString() === bookingId)
+    if (!updatedBooking) {
       throw new Error('Такая запись не найдена')
     }
-    service.appointment.booking[bookingIndex].dateStart = updatedBooking.dateStart
-    service.appointment.booking[bookingIndex].dateEnd = updatedBooking.dateEnd
-    await service.save()
-    return service.appointment.booking[bookingIndex]
+
+    return updatedBooking
   }
   async remove(serviceId: string, bookingId: string) {
     const service = await serviceModel.findByIdAndUpdate(serviceId, {
